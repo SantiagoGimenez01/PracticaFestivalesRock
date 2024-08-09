466e6037b05d96e68dbb82e513c0c91cbe2b977d
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-08-09 20:48:18 -0300

    Consignas y armado de la base de conocimiento

---
+++ festivalDeRock.pl
@@ -0,0 +1,29 @@
+% festival(NombreDelFestival, Bandas, Lugar).
+% Relaciona el nombre de un festival con la lista de los nombres de bandas que tocan en él y el lugar dónde se realiza.
+festival(lollapalooza, [gunsAndRoses, theStrokes, littoNebbia], hipodromoSanIsidro).
+
+% lugar(nombre, capacidad, precioBase).
+% Relaciona un lugar con su capacidad y el precio base que se cobran las entradas ahí.
+lugar(hipodromoSanIsidro, 85000, 3000).
+
+% banda(nombre, nacionalidad, popularidad).
+% Relaciona una banda con su nacionalidad y su popularidad.
+banda(gunsAndRoses, eeuu, 69420).
+
+% entradaVendida(NombreDelFestival, TipoDeEntrada).
+% Indica la venta de una entrada de cierto tipo para el festival 
+% indicado.
+% Los tipos de entrada pueden ser alguno de los siguientes: 
+%     - campo
+%     - plateaNumerada(Fila)
+%     - plateaGeneral(Zona).
+entradaVendida(lollapalooza, campo).
+entradaVendida(lollapalooza, plateaNumerada(1)).
+entradaVendida(lollapalooza, plateaGeneral(zona2)).
+
+% plusZona(Lugar, Zona, Recargo)
+% Relacion una zona de un lugar con el recargo que le aplica al precio de las plateas generales.
+plusZona(hipodromoSanIsidro, zona1, 1500).
+
+% Punto 1
+% itinerante/1: Se cumple para los festivales que ocurren en más de un lugar, pero con el mismo nombre y las mismas bandas en el mismo orden.
