e65099c50a624afa4bdd4c427343493f5bdf9a84
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-08-09 21:25:24 -0300

    Puntos 3 y 4

--- festivalDeRock.pl
+++ festivalDeRock.pl
@@ -4,6 +4,7 @@ festival(lollapalooza, [gunsAndRoses, theStrokes, littoNebbia], hipodromoSanIsid
 festival(lollapalooza, [gunsAndRoses, theStrokes, littoNebbia], laRural).
 festival(personalFest, [rombai, losDelEspacio, marama], movistarArena).
 festival(buenosAiresTrap, [modoDiablo, barderos, ysyA], estadioVelez).
+festival(rockAndRoll, [divididos, enanitosVerdes, losRedondos], estadoUnicoDeLaPlata).
 
 % lugar(nombre, capacidad, precioBase).
 % Relaciona un lugar con su capacidad y el precio base que se cobran las entradas ahí.
@@ -12,6 +13,9 @@ lugar(hipodromoSanIsidro, 85000, 3000).
 % banda(nombre, nacionalidad, popularidad).
 % Relaciona una banda con su nacionalidad y su popularidad.
 banda(gunsAndRoses, eeuu, 69420).
+banda(divididos, argentina, 30000).
+banda(enanitosVerdes, argentina, 20000).
+banda(losRedondos, argentina, 50000).
 
 % entradaVendida(NombreDelFestival, TipoDeEntrada).
 % Indica la venta de una entrada de cierto tipo para el festival 
@@ -29,6 +33,8 @@ entradaVendida(buenosAiresTrap, plateaNumerada(1)).
 entradaVendida(buenosAiresTrap, plateaGeneral(zona2)).
 
 entradaVendida(personalFest, campo).
+
+entradaVendida(rockAndRoll, campo).
 
 
 % plusZona(Lugar, Zona, Recargo)
@@ -48,3 +54,20 @@ careta(personalFest).
 careta(Festival):-
     festival(Festival, _, _),
     forall(entradaVendida(Festival, TipoDeEntrada), TipoDeEntrada \= campo).
+
+% Punto 3
+% nacAndPop/1: Un festival es nac&pop si no es careta y todas las bandas que tocan en él son de nacionalidad argentina y tienen popularidad mayor a 1000.
+nacAndPop(Festival):-
+    festival(Festival, Bandas, _),
+    not(careta(Festival)),
+    forall(member(Banda, Bandas), banda(Banda, argentina, _)).
+
+% Punto 4
+% sobrevendido/1: Se cumple para los festivales que vendieron más entradas que la capacidad del lugar donde se realizan.
+% Nota: no hace falta contemplar si es un festival itinerante.
+sobrevendido(Festival):-
+    festival(Festival, _, Lugar),
+    lugar(Lugar, Capacidad, _),
+    findall(TipoDeEntrada, entradaVendida(Festival, TipoDeEntrada), EntradasVendidas),
+    length(EntradasVendidas, Cantidad),
+    Cantidad > Capacidad.
